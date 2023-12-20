72ba793cae7846c2e1fcd4b90d991d96742d1ef6
UĞURCAN <UURCAN@LAPTOP-SAB3RQO3>
2023-12-20 16:06:15 +0300

    Proje dosyası ekle.

---
+++ trafik ışıklarının işlemleri/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trafik_ışıklarının_işlemleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //trafik_ışıklarının_işlemleri
+            Console.WriteLine("trafik ışıklarının işlemleri");
+            Console.WriteLine("renk giriniz");      
+             string renk = Console.ReadLine();
+            switch (renk)
+            {
+                case "kırmızı":
+                    Console.WriteLine("dur");
+                    break;
+
+                case "sarı":
+                    Console.WriteLine("hazırlan");
+                    break;
+                    
+                case "yeşil":
+                    Console.WriteLine("geçebilirsiniz");
+                    break;  
+
+                default:
+                    Console.WriteLine("geçersiz renk girişi");
+                    break;
+            }
+            Console.ReadKey();  
+              
+            
+
+
+
+
+
+        }
+    }
+}
